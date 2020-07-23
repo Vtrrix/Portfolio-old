589889f65df247ed4f1af86e9073abedc5d84d3e
VIVEK SONY <45513498+Vtrrix@users.noreply.github.com>
2020-07-23 20:40:41 +0530

    new portfolio merged

--- app.js
+++ app.js
@@ -1,347 +1,361 @@
-// const preload= document.querySelector('.preload');
+//----------------------Arrow navigation---------------------------------
+document.getElementsByClassName("right")[0].addEventListener("click", () => {
+  window.scroll({
+    left: window.innerWidth + 1,
+    behavior: "smooth",
+  });
+});
+document.getElementsByClassName("right")[1].addEventListener("click", () => {
+  window.scroll({
+    left: window.innerWidth + 1,
+    behavior: "smooth",
+  });
+});
+document.getElementsByClassName("left")[0].addEventListener("click", () => {
+  window.scroll({
+    left: -window.innerWidth - 1,
+    behavior: "smooth",
+  });
+});
+document.getElementsByClassName("left")[1].addEventListener("click", () => {
+  window.scroll({
+    left: -window.innerWidth - 1,
+    behavior: "smooth",
+  });
+});
+document.getElementsByClassName("up")[0].addEventListener("click", () => {
+  window.scroll({
+    top: -window.innerHeight - 1,
+    behavior: "smooth",
+  });
+});
+document.getElementsByClassName("up")[1].addEventListener("click", () => {
+  window.scroll({
+    top: -window.innerHeight - 1,
+    behavior: "smooth",
+  });
+});
+document.getElementsByClassName("down")[0].addEventListener("click", () => {
+  window.scroll({
+    top: window.innerHeight + 1,
+    behavior: "smooth",
+  });
+});
+document.getElementsByClassName("down")[1].addEventListener("click", () => {
+  window.scroll({
+    top: window.innerHeight + 1,
+    behavior: "smooth",
+  });
+});
+anime({
+  targets: [".arrow"],
+  loop: true,
+  opacity: 1,
+  easing: "easeInOutSine",
+  duration: 1400,
+  direction: "alternate",
+});
 
+//--------------------------------LANDING-------------------------------------------------
+
+let burger = document.getElementById("burger");
+let navbar = document.getElementById("navbar");
 
 window.addEventListener("load", () => {
-  window.scrollTo(0,0);
-  // preload.style.height=0;
-  // preload.style.width=0;
-  
-})
-//side buttons
-let butt1 = document.querySelector(".butt1");
-let butt2 = document.querySelector(".butt2");
-let butt3 = document.querySelector(".butt3");
-let butt4 = document.querySelector(".butt4");
-let butt5 = document.querySelector(".butt5");
-let butt6 = document.querySelector(".butt6");
+  anime({
+    targets: [".line "],
+    height: {
+      value: ["33.33%", "15%"],
+      duration: 1200,
+      delay: 700,
+    },
+    margin: {
+      value: "3% 0%",
+      duration: 1500,
+      delay: 1900,
+    },
+  });
+  anime({
+    targets: "#burger",
+    height: "40px",
+    width: "45px",
+    left: "2.5vw",
+    top: "3vh",
+    delay: 1000,
+    easing: "easeInOutQuad",
+  });
+});
 
-let burger=document.querySelector(".burger");
-let line1=document.querySelector(".line1");
-let line2=document.querySelector(".line2");
-let line3=document.querySelector(".line3");
-let navbar = document.querySelector(".navbar");
-let flag_for_nav=false;//to know if the nav is open or close
-//flag=true is open 
-
-
-burger.addEventListener("click", ()=>{
-  if(! flag_for_nav){
-    navbar.style.width='45%';
-    navbar.style.height='75%';
-    flag_for_nav=true;
+//Background sphere animation
+function fitElementToParent(el, padding) {
+  var timeout = null;
+  function resize() {
+    if (timeout) clearTimeout(timeout);
+    anime.set(el, { scale: 1 });
+    var pad = padding || 0;
+    var parentEl = el.parentNode;
+    var elOffsetWidth = el.offsetWidth - pad;
+    var parentOffsetWidth = parentEl.offsetWidth;
+    var ratio = parentOffsetWidth / elOffsetWidth;
+    timeout = setTimeout(anime.set(el, { scale: ratio }), 10);
   }
-  else{
-    navbar.style.width='0';
-    navbar.style.height='0';
-    flag_for_nav=false;
-  }
-})
-burger.addEventListener("blur", ()=>{
-  navbar.style.width='0';
-  navbar.style.height='0';
-  flag_for_nav=false;
-})
-
-const slide = document.querySelector('.about');
-const little = document.querySelector('.little');
-const info1 = document.querySelector('.info1');
-const info2 = document.querySelector('.info2');
-const butt_contain1 = document.querySelector('.butt_contain1');
-const butt = document.querySelector('.butt');
-var ctrl = new ScrollMagic.Controller();
-var window_width=window.innerWidth;
-
-
-const tl =new TimelineMax();
-tl.fromTo(butt,0.5,{height:"0"},{height:"100%", ease:Power2.easeInOut} );
-if(window_width >= 450)
-{
-  tl.fromTo(slide,1.2,{x:"-100%"},{x:"-7.2%" , ease:Power2.easeInOut} )
-}
-tl.fromTo(little,1.5,{opacity:"0" , y:"5"},{opacity:"1" , y:"0", ease:Power2.easeInOut} ,'-=0.7')
-tl.fromTo(info1,1,{opacity:"0", y:"5"},{opacity:"1" , y:"0", ease:Power2.easeInOut} ,'-=1')
-tl.fromTo(info2,1,{opacity:"0", y:"5"},{opacity:"1" , y:"0", ease:Power2.easeInOut} ,'-=0.5' )
-tl.fromTo(butt_contain1,0.2,{border:"0"},{border:"2px solid rgb(230, 0, 0)", ease:Power2.easeInOut} )
-tl.fromTo(butt_contain1,0.2,{width:"0"},{width:"200px", ease:Power2.easeInOut} )
-tl.fromTo(butt,0.5,{height:"0"},{height:"100%", ease:Power2.easeInOut} );
-
-
-
-const skills=document.querySelector('.skills');
-const skill1 = document.querySelector('.skill1');
-const skill2 = document.querySelector('.skill2');
-const skill3 = document.querySelector('.skill3');
-const skill1img = document.querySelector('.skill1 img');
-const skill2img = document.querySelector('.skill2 img');
-const skill3img = document.querySelector('.skill3 img');
-const stext= document.querySelector('.stext');
-const t2 =new TimelineMax();
-
-if(window_width >= 880)
-{
-  t2.fromTo(skill3,1.3,{x:"-80%"},{x:"0%" , ease:Power2.easeInOut} )
-  t2.fromTo(skill3img,1,{opacity:"0"},{opacity:"1" , ease:Power2.easeInOut} ,'-=1')
-  t2.fromTo(skill2,1.3,{x:"-85%"},{x:"0%" , ease:Power2.easeInOut},'-=1')
-  t2.fromTo(skill2img,1,{opacity:"0"},{opacity:"1" , ease:Power2.easeInOut} ,'-=1')
-  t2.fromTo(skill1,1.3,{x:"-90%"},{x:"0%" , ease:Power2.easeInOut},'-=1' )
-  t2.fromTo(skill1img,1,{opacity:"0"},{opacity:"1" , ease:Power2.easeInOut} ,'-=1' );
-
-  t2.fromTo(stext,1,{opacity:"0"},{opacity:"1" , ease:Power2.easeInOut} ,'-=0.5' )
-  t2.fromTo(stext,1,{y:"10px"},{y:"0" , ease:Power2.easeInOut} ,'-=1.2' );
-}
-if(window_width < 880)
-{
-  t2.fromTo(stext,1,{opacity:"0"},{opacity:"1" , ease:Power2.easeInOut} ,'-=1.5' )
-  t2.fromTo(stext,1,{y:"10px"},{y:"0" , ease:Power2.easeInOut} ,'-=1.7' );
-
-  t2.fromTo(skill1,1.3,{x:"-100%"},{x:"0%" , ease:Power2.easeInOut} ,'-=1')
-  t2.fromTo(skill1img,1,{opacity:"0"},{opacity:"1" , ease:Power2.easeInOut} ,'-=1')
-  t2.fromTo(skill2,1.3,{x:"-105%"},{x:"0%" , ease:Power2.easeInOut},'-=1')
-  t2.fromTo(skill2img,1,{opacity:"0"},{opacity:"1" , ease:Power2.easeInOut} ,'-=1')
-  t2.fromTo(skill3,1.3,{x:"-100%"},{x:"0%" , ease:Power2.easeInOut},'-=1' )
-  t2.fromTo(skill3img,1,{opacity:"0"},{opacity:"1" , ease:Power2.easeInOut} ,'-=1' );
-
- 
-}
-const Projects=document.querySelector('.Projects');
-const projects_title=document.querySelector('.projects_title');
-const contain1=document.querySelector('.contain1');
-const contain2=document.querySelector('.contain2');
-const contain3=document.querySelector('.contain3');
-
-const t3 =new TimelineMax();
-
-t3.fromTo(projects_title,1,{y:"5%",opacity:"0"},{y:"0%",opacity:"1", ease:Power2.easeInOut},'-=0.5'  )
-if(window_width>1330)
-{
-  t3.fromTo(contain1,0.5,{height:"0"},{height:"450px", ease:Power2.easeInOut} )
-  t3.fromTo(contain2,0.5,{height:"0"},{height:"450px", ease:Power2.easeInOut} )
-  t3.fromTo(contain3,0.5,{height:"0"},{height:"450px", ease:Power2.easeInOut} )
-}
-else{
-  t3.fromTo(contain1,0.5,{y:"5%",opacity:"0"},{y:"0%",opacity:"1", ease:Power2.easeInOut} )
-  t3.fromTo(contain2,0.5,{y:"5%",opacity:"0"},{y:"0%",opacity:"1", ease:Power2.easeInOut} )
-  t3.fromTo(contain3,0.5,{y:"5%",opacity:"0"},{y:"0%",opacity:"1", ease:Power2.easeInOut} )
+  resize();
+  window.addEventListener("resize", resize);
 }
 
-const Contact=document.querySelector('.Contact')
-const t4 =new TimelineMax();
-const contact_title=document.querySelector('.contact_title');
-const name=document.querySelector('.name');
-const mail=document.querySelector('.mail');
-const subject=document.querySelector('.subject');
-const msg=document.querySelector('.msg');
+var sphereAnimation = (function () {
+  var sphereEl = document.querySelector(".sphere-animation");
+  var spherePathEls = sphereEl.querySelectorAll(".sphere path");
+  var pathLength = spherePathEls.length;
+  var aimations = [];
 
-const butt_contain2 = document.querySelector('.butt_contain2');
-const submit=document.querySelector('.submit');
+  fitElementToParent(sphereEl);
 
+  var breathAnimation = anime({
+    begin: function () {
+      for (var i = 0; i < pathLength; i++) {
+        aimations.push(
+          anime({
+            targets: spherePathEls[i],
+            stroke: {
+              value: ["rgba(27,3,163,1)", "rgba(80,80,80,.35)"],
+              duration: 500,
+            },
+            translateX: [2, -4],
+            translateY: [2, -4],
+            easing: "easeOutQuad",
+            autoplay: false,
+          })
+        );
+      }
+    },
+    update: function (ins) {
+      aimations.forEach(function (animation, i) {
+        var percent = (1 - Math.sin(i * 0.35 + 0.0022 * ins.currentTime)) / 2;
+        animation.seek(animation.duration * percent);
+      });
+    },
+    duration: Infinity,
+    autoplay: false,
+  });
 
-const uname=document.querySelector('.uname');
-const umail=document.querySelector('.umail');
-const usubject=document.querySelector('.usubject');
-const umsg=document.querySelector('.umsg');
-
-
-t4.fromTo(contact_title,1,{y:"5%",opacity:"0"},{y:"0%",opacity:"1", ease:Power2.easeInOut}  )
-
-t4.fromTo(name,1.5,{width:"0%"},{width:"100%", ease:Power2.easeInOut} ,'-=1'  )
-t4.fromTo(uname,0.5,{y:"5%",opacity:"0"},{y:"0%",opacity:"1", ease:Power2.easeInOut},'-=1'  )
-
-t4.fromTo(mail,1.5,{width:"0%"},{width:"100%", ease:Power2.easeInOut} ,'-=1'  )
-t4.fromTo(umail,0.5,{y:"5%",opacity:"0"},{y:"0%",opacity:"1", ease:Power2.easeInOut},'-=1'  )
-
-t4.fromTo(subject,1.5,{width:"0%"},{width:"100%", ease:Power2.easeInOut} ,'-=1'  )
-t4.fromTo(usubject,0.5,{y:"5%",opacity:"0"},{y:"0%",opacity:"1", ease:Power2.easeInOut},'-=1'  )
-
-t4.fromTo(msg,1.5,{width:"0%"},{width:"100%", ease:Power2.easeInOut} ,'-=1'  )
-t4.fromTo(umsg,0.5,{y:"5%",opacity:"0"},{y:"0%",opacity:"1", ease:Power2.easeInOut},'-=1'  )
-
-t4.fromTo(butt_contain2,0.2,{border:"0"},{border:"2px solid rgb(230, 0, 0)", ease:Power2.easeInOut} ,'-=0.2' )
-t4.fromTo(butt_contain2,0.2,{width:"0"},{width:"200px", ease:Power2.easeInOut} )
-t4.fromTo(submit,0.5,{height:"0"},{height:"100%", ease:Power2.easeInOut} );
-
-name.addEventListener("click",()=>{
-  uname.style.top='0px';
-})
-name.addEventListener("",()=>{
-  uname.style.top='100px';
-})
-mail.addEventListener("click",()=>{
-  umail.style.top='0px';
-    
-})
-subject.addEventListener("click",()=>{
-  usubject.style.top='0px';
-    
-})
-
-
-new ScrollMagic.Scene({
-    triggerElement: slide,
-    triggerHook: 0.25
-  })
-    .setTween(tl)
-    .addIndicators({
-      colorTrigger: "white",
-      colorStart: "white",
-      colorEnd: "white",
-      indent: 20
+  var introAnimation = anime
+    .timeline({
+      autoplay: false,
     })
-    .addTo(ctrl);
-new ScrollMagic.Scene({
-    triggerElement: skills,
-    triggerHook: 0.25
-  })
-    .setTween(t2)
-    .addIndicators({
-      colorTrigger: "white",
-      colorStart: "white",
-      colorEnd: "white",
-      indent: 20
-    })
-    .addTo(ctrl);
+    .add(
+      {
+        targets: spherePathEls,
+        strokeDashoffset: {
+          value: [anime.setDashoffset, 0],
+          duration: 3900,
+          easing: "easeInOutCirc",
+          delay: anime.stagger(190, { direction: "reverse" }),
+        },
+        duration: 2000,
+        delay: anime.stagger(60, { direction: "reverse" }),
+        easing: "linear",
+      },
+      0
+    );
 
-new ScrollMagic.Scene({
-      triggerElement: Projects,
-      triggerHook: 0.25
-   })
-    .setTween(t3)
-    .addIndicators({
-      colorTrigger: "white",
-      colorStart: "white",
-      colorEnd: "white",
-      indent: 20
-    })
-    .addTo(ctrl);
+  var shadowAnimation = anime(
+    {
+      targets: "#sphereGradient",
+      x1: "25%",
+      x2: "25%",
+      y1: "0%",
+      y2: "75%",
+      duration: 30000,
+      easing: "easeOutQuint",
+      autoplay: false,
+    },
+    0
+  );
 
-new ScrollMagic.Scene({
-      triggerElement: Contact,
-      triggerHook: 0.25
-    })
-    .setTween(t4)
-    .addIndicators({
-      colorTrigger: "white",
-      colorStart: "white",
-      colorEnd: "white",
-      indent: 20
-    })
-    .addTo(ctrl);
-
-
-
-
-let active = document.querySelector(".active");
-
-
-let cur_but_num = 1;
-function changePrevButton(){
-  if(cur_but_num == 1)
-  {
-    butt1.style.backgroundColor = "transparent";
+  function init() {
+    introAnimation.play();
+    breathAnimation.play();
+    shadowAnimation.play();
   }
-  else if(cur_but_num == 2)
-  {
-    butt2.style.backgroundColor = "transparent";
+
+  init();
+})();
+
+//navbar appear animation
+
+let nav = false;
+
+burger.addEventListener("click", () => {
+  nav = !nav;
+
+  if (nav) {
+    navbar.style.width = "100%";
+    navbar.style.height = "100%";
+    burger.style.cursor = "default";
+
+    anime({
+      targets: [".line "],
+
+      margin: {
+        value: ["3% , 0%", "0% 0%"],
+        duration: 300,
+        delay: 100,
+        easing: "linear",
+      },
+      height: {
+        value: ["15%", "33.33%"],
+        duration: 100,
+        delay: 550,
+        easing: "linear",
+      },
+    });
+    anime({
+      targets: "#burger",
+      height: "100%",
+      width: "100%",
+      left: "0",
+      top: "0",
+      easing: "easeInOutQuad",
+    });
+    anime({
+      targets: "#back",
+      width: {
+        value: "3.5rem",
+        delay: 1500,
+      },
+      rotate: {
+        value: 360,
+        delay: 1700,
+        duration: 700,
+        easing: "easeInOutSine",
+      },
+    });
+  } else {
+    anime({
+      targets: "#back",
+      rotate: {
+        value: 0,
+        duration: 1000,
+        easing: "easeInOutSine",
+      },
+      width: {
+        value: "0",
+        delay: 500,
+        duration: 1000,
+      },
+    });
+
+    anime({
+      targets: [".line "],
+      margin: {
+        value: ["0% 0%", "3% 0%"],
+        duration: 500,
+        delay: 1000,
+        easing: "linear",
+      },
+      height: {
+        value: ["33.33%", "15%"],
+        duration: 100,
+        delay: 500,
+        easing: "linear",
+      },
+    });
+    burger.style.height = "40px";
+    burger.style.width = "45px";
+    anime({
+      targets: "#burger",
+      left: "2.5vw",
+      top: "3vh",
+      duration: "500",
+      easing: "easeInOutQuad",
+    });
+    navbar.style.width = "0";
+    navbar.style.height = "0";
+    burger.style.cursor = "pointer";
   }
-  else if(cur_but_num == 3)
-  {
-    butt3.style.backgroundColor = "transparent";
+});
+
+//--------------------------------ABOUT-------------------------------------------------
+
+let about = document.getElementById("about");
+let words = [
+  "Vtrix",
+  "WEB-D",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "ReactJS",
+  "Vue",
+  "SASS",
+  "BootStrap",
+  "MaterialUI",
+  "NodeJS",
+  "MongoDB",
+];
+window.addEventListener("load", function () {
+  let randoms = document.getElementsByClassName("randoms");
+  for (let i = 0; i < randoms.length; i++) {
+    changeWord(randoms[i]);
   }
-  else if(cur_but_num == 4)
-  {
-    butt4.style.backgroundColor = "transparent";
-  }
-  else if(cur_but_num == 5)
-  {
-    butt5.style.backgroundColor = "transparent";
-  }
-  else if(cur_but_num == 6)
-  {
-    butt6.style.backgroundColor = "transparent";
-  }
+});
+
+function changeWord(a) {
+  a.style.opacity = "0.5";
+  a.innerHTML = words[getRandomInt(0, words.length - 1)];
+  setTimeout(function () {
+    a.style.opacity = "1";
+  }, 600);
+  setTimeout(function () {
+    changeWord(a);
+  }, getRandomInt(800, 1000));
 }
-butt1.style.backgroundColor = "white";
-butt1.addEventListener("click", function(){
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
-  changePrevButton();
-  cur_but_num = 1;
-  this.style.backgroundColor = "white";
-})
-butt2.addEventListener("click", function(){
-  window.scrollTo({
-    top: window.innerHeight+1,
-    left: 0,
-    behavior: 'smooth'
-  });
-  changePrevButton();
-  cur_but_num = 2;
-  this.style.backgroundColor = "white";
-})
-butt3.addEventListener("click", function(){
-  window.scrollTo({
-    top: 2*window.innerHeight+1,
-    left: 0,
-    behavior: 'smooth'
-  });
-  changePrevButton();
-  cur_but_num = 3;
-  this.style.backgroundColor = "white";
-})
-butt4.addEventListener("click", function(){
-  window.scrollTo({
-    top: 3*window.innerHeight+1,
-    left: 0,
-    behavior: 'smooth'
-  });
-  changePrevButton();
-  cur_but_num = 4;
-  this.style.backgroundColor = "white";
-})
-butt5.addEventListener("click", function(){
-  window.scrollTo({
-    top: 4*window.innerHeight+1,
-    left: 0,
-    behavior: 'smooth'
-  });
-  changePrevButton();
-  cur_but_num = 5;
-  this.style.backgroundColor = "white";
-})
-butt6.addEventListener("click", function(){
-  window.scrollTo({
-    top: 5*window.innerHeight+1,
-    left: 0,
-    behavior: 'smooth'
-  });
-  changePrevButton();
-  cur_but_num = 6;
-  this.style.backgroundColor = "white";
-})
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+//-----------------------------------------------Contact Form---------------------------------------
+let inputClick = (ele) => {
+  ele.children[0].style.top = 0;
+  ele.children[0].style.fontSize = "1.2rem";
+  ele.children[0].style.color = "#3215d6";
+  ele.children[0].style.textShadow = "0px 0px 12px #3215d6";
+  ele.children[1].style.borderBottom = "1px solid #3215d6";
+};
+let inputBlur = (ele) => {
+  ele.children[0].style.top = "15px";
+  ele.children[0].style.fontSize = "1rem";
+  ele.children[0].style.color = "whitesmoke";
+  ele.children[0].style.textShadow = "0px 0px 0px #000000";
+  ele.children[1].style.borderBottom = "1px solid whitesmoke";
+};
 
-// let initScrollPos = window.pageYOffset;
-// let scrollOffset = 0;
-// window.addEventListener("scroll", function(){
-//   let scrollPosNow = window.pageYOffset;
-//   if(scrollPosNow > initScrollPos)
-//   {
-//     scrollOffset+=1;
-//     window.scrollTo({
-//       top: scrollOffset*window.innerHeight+1
-//     })
-//     initScrollPos = scrollOffset*window.innerHeight+1;
-//   }
-//   else{
-//     scrollOffset-=1;
-//     window.scrollTo({
-//       top: scrollOffset*window.innerHeight+1
-//     })
-//     initScrollPos = scrollOffset*window.innerHeight+1;
-//   }
-// });
+let inputs = document.querySelectorAll(".formFeild");
 
+inputs[0].children[1].addEventListener("click", () => {
+  inputClick(inputs[0]);
+});
+inputs[1].children[1].addEventListener("click", () => {
+  inputClick(inputs[1]);
+});
+inputs[2].children[1].addEventListener("click", () => {
+  inputClick(inputs[2]);
+});
+inputs[3].children[1].addEventListener("click", () => {
+  inputClick(inputs[3]);
+});
+
+inputs[0].children[1].addEventListener("blur", () => {
+  inputBlur(inputs[0]);
+});
+inputs[1].children[1].addEventListener("blur", () => {
+  inputBlur(inputs[1]);
+});
+
+inputs[2].children[1].addEventListener("blur", () => {
+  inputBlur(inputs[2]);
+});
+inputs[3].children[1].addEventListener("blur", () => {
+  inputBlur(inputs[3]);
+});
